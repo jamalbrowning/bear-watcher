c76915cabf8b1f3ca053a2b79c3953813bede6a1
Jim Browning <jamalebrowning@gmail.com>
2020-06-02 18:02:51 -0500

    added button to cards

--- javascripts/Components/river.js
+++ javascripts/Components/river.js
@@ -1,1 +1,26 @@
 import bearData from '../Helpers/Data/bears.js'
+import utils from '../Helpers/utils.js'
+
+const bearCardBuilder = () => {
+const bears = bearData.getBears();
+  let domString = ''
+  
+  bears.forEach(bear => {
+    domString += `
+      <div class="card" style="width: 18rem;">
+        <img src="${bear.imageUrl}" class="bear-${bear.name}-card" alt="brown bear">
+        <div class="card-body">
+          <h5 class="card-name">${bear.name}</h5>
+        </div>
+        <div class="card-button">
+          <button class="btn btn-danger"><i class="fas fa-fish"></i></button>
+        </div>
+      </div>`
+      });
+  
+  utils.printToDom ('#river-container ' , domString)
+  };
+  
+
+  export default {bearCardBuilder}
+
